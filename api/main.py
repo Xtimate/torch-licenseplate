e803898555a9294fc4f04becf16acd3968046086
Xtimate <lordquestionmark.discord@gmail.com>
2026-05-07 21:05:31 +0200

    added localhost http to cors

--- api/main.py
+++ api/main.py
@@ -95,7 +95,12 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)  # type: ignore
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://xtimate.github.io", "http://localhost:5173"],
+    allow_origins=[
+        "https://xtimate.github.io",
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "null",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
